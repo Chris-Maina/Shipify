7620f63906dd3b6efc8e88042193bd12925ec089
Chris Maina <chrismaina@Chris-MacBook.local>
2020-03-30 23:19:25 +0300

    add getId and addOne helpers

--- src/containers/ShipmentScreen/helpers.js
+++ src/containers/ShipmentScreen/helpers.js
@@ -1,12 +1,28 @@
-// export const search = (searchText, data) => {
-//   if (searchText) {
-//     const results = data
-//       .filter(activity => Object.values(activity)
-//         .some(value => value && value.toString().toLowerCase().includes(searchText.toLowerCase())));
-//     return results;
-//   }
-//   return data;
-// };
+export const getId = digits => {
+  digits = digits.split('')
+  if (digits[digits.length -1] < 9) {
+    digits[digits.length - 1]++;
+    return digits.join('');
+  }
+  return addOne(digits, digits.length - 1);
+};
+
+
+const addOne = (digits, lastPosition) => {
+  // if there is no digit at the lastPosition, add 1
+  if (!digits[lastPosition]) {
+    digits.unshift(1);
+    return digits.join('');
+  }
+  // if the digit at the lastPosition is less than 9, increment
+  if (digits[lastPosition] < 9) {
+    digits[lastPosition]++;
+    return digits.join('');
+  }
+  // add 0 to the last position
+  digits[lastPosition] = 0;
+  return addOne(digits, lastPosition - 1);
+}
 
 export const search = (searchText, data) =>
   data.reduce((acc, next) => {
