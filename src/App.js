87f32f97fb515288f2c534468a8f8c6848c1a3c7
Chris Maina <chrismaina@Chris-MacBook.local>
2020-04-07 17:36:58 +0300

    add signin route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Suspense, lazy } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import PageContainer from './containers/PageContainer';
+const LoginScreen = lazy(() => import('./containers/LoginScreen'));
+const PageContainer = lazy(() => import('./containers/PageContainer'));
 const RedirectScreen = lazy(() => import('./containers/RedirectScreen'));
 const ShipmentScreen = lazy(() => import('./containers/ShipmentScreen'));
 const ShipmentDetailsScreen = lazy(() => import('./containers/ShipmentDetailsScreen'));
@@ -10,21 +11,23 @@ const ShipmentDetailsScreen = lazy(() => import('./containers/ShipmentDetailsScr
 function App() {
   return (
     <BrowserRouter>
+     <Suspense fallback={<CircularProgress style={{ margin: '0 auto', display: 'table', lineHeight: 40 }} />}>
       <Switch>
+        <Route exact path="/signin" component={LoginScreen} />
         <Route
           render={() => (
             <PageContainer>
-              <Suspense fallback={<CircularProgress style={{ margin: '0 auto', display: 'table', lineHeight: 40 }} />}>
                 <Switch>
                   <Route exact path="/" component={RedirectScreen} />
                   <Route exact path="/shipments" component={ShipmentScreen} />
                   <Route exact path="/shipments/:id" component={ShipmentDetailsScreen} />
                 </Switch>
-              </Suspense>
+              
             </PageContainer>
           )}>
         </Route>
       </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
