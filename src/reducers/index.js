2ddbc3a9820ec019a218f887cf055caa04f9fb20
Chris Maina <chrismaina@Chris-MacBook.local>
2020-04-11 20:50:05 +0300

    add global, signUp, signIn reducer and sagas

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
+import global from '../containers/PageContainer/store/reducer';
+import signInScreen from '../containers/LoginScreen/store/reducer';
+import signUpScreen from '../containers/SignUpScreen/store/reducer';
 import shipmentScreen from '../containers/ShipmentScreen/store/reducer';
 import shipmentDetailScreen from '../containers/ShipmentDetailsScreen/store/reducer';
 
 const rootReducer = combineReducers({
+  global,
+  signInScreen,
+  signUpScreen,
   shipmentScreen,
   shipmentDetailScreen
 });
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,15 @@
 import { all } from 'redux-saga/effects';
+import signInSaga from '../containers/LoginScreen/store/saga';
+import signUpSaga from '../containers/SignUpScreen/store/saga';
+import globalSaga from '../containers/PageContainer/store/sagas';
 import shipmentSaga from '../containers/ShipmentScreen/store/sagas';
 import shipmentDetailsSaga from '../containers/ShipmentDetailsScreen/store/sagas';
 
 export default function* rootSaga() {
   yield all([
+    ...globalSaga,
+    ...signUpSaga,
+    ...signInSaga,
     ...shipmentSaga,
     ...shipmentDetailsSaga
   ])
